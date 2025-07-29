8f35daff4687eff8a15b84397091f30ee7c060f3
dkkinyua <denzelkinyua11@gmail.com>
2025-07-29 22:42:12 +0300

    Commit: Kafka producer and consumer set up Producer set up to receive real time data from Binance Websockets via websocket-client package Producer in turn sends data into Kafka topic hosted on Confluent for storage Consumer is set to stream data from Kafka into a sink Postgres database for structured storage and downstream use cases Consumer uses PySpark's Structured Streaming with readStream and writeStream for streaming
    Awaiting actions:
    Containerization of the producer and consumer for task automation to run every minute as Airflow cannot handle streaming well due to Websocket's ws.run_forever() function
    
    Signed by: Denzel Kinyua, Data Engineer

---
+++ scripts/consumer.py
@@ -0,0 +1,89 @@
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, from_json
+from pyspark.sql.types import BooleanType, LongType, StructType, StringType
+
+load_dotenv()
+
+spark = SparkSession.builder \
+            .appName("SparkPostgresConsumer") \
+            .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.1") \
+            .config("spark.jars", "/home/deecodes/jars/postgresql-42.7.7.jar") \
+            .getOrCreate()
+
+def transform_data():
+    topic = 'binance-topic'
+
+    schema = StructType() \
+        .add("close", StringType()) \
+        .add("open", StringType()) \
+        .add("high", StringType()) \
+        .add("low", StringType()) \
+        .add("interval", StringType()) \
+        .add("klineClosed", BooleanType()) \
+        .add("symbol", StringType()) \
+        .add("volume", StringType()) \
+        .add("numTrades", LongType()) \
+        .add("closeTime", LongType()) \
+        .add("startTime", LongType())
+    
+    df = spark \
+        .readStream \
+        .format('kafka') \
+        .option('kafka.bootstrap.servers', os.getenv("BOOTSTRAP_SERVER")) \
+        .option('subscribe', topic) \
+        .option('startingOffsets', 'earliest') \
+        .option('kafka.security.protocol', 'SASL_SSL') \
+        .option('kafka.sasl.mechanism', 'PLAIN') \
+        .option('kafka.sasl.jaas.config', f'org.apache.kafka.common.security.plain.PlainLoginModule required username="{os.getenv("KAFKA_API_KEY")}" password="{os.getenv("KAFKA_SECRET_KEY")}";') \
+        .load()
+    
+    json_df = df.selectExpr("CAST (value AS STRING) as parsed_json") \
+                .select(from_json("parsed_json", schema).alias("data")) \
+                .select("data.*")
+
+    new_df = json_df.select(
+        col("close").cast("float"), 
+        col("high").cast("float"), 
+        col("low").cast("float"), 
+        col("open").cast("float"), 
+        col("volume").cast("float"), 
+        "symbol", "interval", "numTrades", 
+        col("klineClosed").alias("isKlineClosed"),
+        (col("closeTime") / 1000).cast("timestamp").alias("closetime"),
+        (col("startTime") / 1000).cast("timestamp").alias("starttime")
+    )
+    
+    return new_df
+
+def write_each_batch(batch_df, epoch_id):
+    properties = {
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "driver": "org.postgresql.Driver"
+    }
+    try:
+        batch_df.write \
+                .jdbc(url=os.getenv("DB_URL"), table="testcryptostreams", mode="append", properties=properties)
+    except Exception as e:
+        print(f"Error writing batch data: {e}")
+    
+def load():
+    df = transform_data()
+    path = f"/tmp/binance_streaming/stream_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    try:
+        df.writeStream \
+        .foreachBatch(write_each_batch) \
+        .outputMode("append") \
+        .option('checkpointLocation', path) \
+        .start() \
+        .awaitTermination()
+
+        print("Data loaded into Postgres successfully!")
+    except Exception as e:
+        print(f"Error loading data to db: {e}")
+
+if __name__ == '__main__':
+    load()
---
+++ scripts/producer.py
@@ -0,0 +1,84 @@
+import os
+import rel
+import json
+from confluent_kafka import Producer
+from dotenv import load_dotenv
+from websocket import WebSocketApp
+
+load_dotenv()
+
+config = {
+    'bootstrap.servers': os.getenv("BOOTSTRAP_SERVER"),
+    'sasl.username': os.getenv("KAFKA_API_KEY"),
+    'sasl.password': os.getenv("KAFKA_SECRET_KEY"),
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanisms': 'PLAIN'
+}
+
+BASE_URL = "wss://fstream.binance.com"
+symbols = [
+    "btcusdt",   
+    "ethusdt",
+    "bnbusdt",
+    "xrpusdt",
+    "solusdt",
+    "adausdt",
+    "dogeusdt",
+    "maticusdt",
+    "dotusdt",
+    "avaxusdt"
+]
+
+producer = Producer(config)
+
+# websocket helper funcs
+def on_open(ws):
+        print("Websocket Connection open to receive connections")
+
+def on_close(ws, close_status_code, close_msg):
+    print("Connection closed", close_status_code, close_msg)
+
+def on_error(ws, error):
+    print(f"Error: {error}")
+
+def on_message(ws, message):
+    data = json.loads(message)
+    kline = data['data']['k']
+
+    kline_data = {
+        'symbol': kline['s'],
+        'interval': kline["i"],
+        'open': kline['o'],
+        'high': kline['h'],
+        'close': kline['c'],
+        'low': kline['l'],
+        'klineClosed': kline['x'],
+        'volume': kline['q'],
+        'numTrades': kline['n'],
+        'startTime': kline['t'],
+        'closeTime': kline['T']
+    }
+    try:
+        producer.produce("binance-topic", json.dumps(kline_data).encode("utf-8"))
+        print("Data sent successfully")
+    except Exception as e:
+         print(f"Error sending data to topic: {e}")
+
+def get_data(BASE_URL, symbols):
+        streams = "/".join([f"{symbol}@kline_1m" for symbol in symbols])
+        url = f"{BASE_URL}/stream?streams={streams}"
+
+        ws = WebSocketApp(
+            url,
+            on_open=on_open,
+            on_message=on_message,
+            on_close=on_close,
+            on_error=on_error
+        )
+
+        ws.run_forever(dispatcher=rel, reconnect=5)
+        rel.signal(2, rel.abort)  
+        rel.dispatch()
+
+if __name__ == '__main__':
+    get_data(BASE_URL, symbols)
